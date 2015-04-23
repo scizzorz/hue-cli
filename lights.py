2f17cad344086f1beddeac04cd5c83c9b96ee889
John Weachock <jweachock@gmail.com>
2015-04-23 04:03:41 -0400

    Change " to '

--- lights.py
+++ lights.py
@@ -22,13 +22,14 @@ HUES = {
 
 parser = argparse.ArgumentParser()
 group = parser.add_mutually_exclusive_group()
-group.add_argument("-c", "--color", choices=list(HUES))
-group.add_argument("-u", "--hue", help="0 - 360", type=int)
-parser.add_argument("-s", "--saturation", help="0 - 100", type=int)
-parser.add_argument("-b", "--brightness", help="0 - 100", type=int)
-parser.add_argument("-l", "--lights", help="comma-separated light indices", default="1,2")
-parser.add_argument("-t", "--time", help="transition time in seconds", default=0.4, type=float)
-parser.add_argument("-o", "--off", help="turn off lights", action="store_true")
+group.add_argument('-c', '--color', choices=list(HUES))
+group.add_argument('-u', '--hue', help='0 - 360', type=int)
+parser.add_argument('-s', '--saturation', help='0 - 100', type=int)
+parser.add_argument('-b', '--brightness', help='0 - 100', type=int)
+parser.add_argument('-l', '--lights', help='comma-separated light indices', default='1,2')
+parser.add_argument('-t', '--time', help='transition time in seconds', default=0.4, type=float)
+parser.add_argument('-o', '--off', help='turn off lights', action='store_true')
+
 args = parser.parse_args()
 
 bridge = phue.Bridge('192.168.1.113')
