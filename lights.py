0cb2c35675b7515b46b864f6620de0b2484980fa
John Weachock <jweachock@gmail.com>
2015-04-22 20:45:02 -0400

    Add some keyword colors

--- lights.py
+++ lights.py
@@ -3,8 +3,26 @@
 import argparse
 import phue
 
+HUES = {
+	'red': 0,
+	'orange': 30,
+	'yellow': 60,
+	'lime': 90,
+	'green': 120,
+	'seafoam': 150,
+	'foam': 150,
+	'cyan': 180,
+	'sky': 210,
+	'blue': 240,
+	'purple': 270,
+	'magenta': 300,
+	'pink': 330,
+}
+
+
 parser = argparse.ArgumentParser()
 parser.add_argument("-u", "--hue", help="the hue", type=int)
+parser.add_argument("-c", "--color", help="the color", type=str)
 parser.add_argument("-s", "--saturation", help="the saturation", type=int)
 parser.add_argument("-b", "--brightness", help="the brightness", type=int)
 parser.add_argument("-l", "--lights", help="the lights", default="1,2")
@@ -16,6 +34,9 @@ bridge = phue.Bridge('192.168.1.113')
 bridge.connect()
 
 package = {}
+
+if args.color is not None and args.color in HUES:
+	args.hue = HUES[args.color]
 
 if args.hue is not None:
 	package['hue'] = int(args.hue * 65535 / 360)
